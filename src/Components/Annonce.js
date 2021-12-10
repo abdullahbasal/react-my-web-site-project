6ed194e6d7f09ba2e0590599ba8adcc402f73779
unknown <1abdullahbasal@gmail.com>
2021-12-10 08:19:03 +0300

    annonce comp added

--- src/Components/Annonce.js
+++ src/Components/Annonce.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import "../styles/components/annonce.css";
+import "../styles/components/Annonce.css";
 import megaphone from "../images/megaphone.png";
+import AddAdvert from "../Components/AddAdvert";
 function Annonce() {
   return (
     <>
-      <Container>
+      <Container className="annonce-container">
         <Row className="justify-content-md-center">
           <Col xs lg="2">
             <img src={megaphone} />
           </Col>
-          <Col md="auto">Variable width content</Col>
-          <Col xs lg="2">
-            3 of 3
+          <Col md="6">
+            <h2>Ücretsiz İlan Ver</h2>
+            <p>
+              Oyun keyfi arkadaşlarla çıkar. Oyun keyfini katlamak, yeni
+              arkadaşlıklar edinmek için web sitemizde ilan verip yeni
+              bağlantılar kurabilirsin. Oyun arkadaşının yanı sıra Hesap Satış,
+              Sunucu Tanıtım ilanları verebilir, Twitch ve YouTube kanalı
+              tanıtımı yapabilirsin!
+            </p>
+          </Col>
+          <Col xs lg="4">
+            <AddAdvert></AddAdvert>
           </Col>
         </Row>
       </Container>
